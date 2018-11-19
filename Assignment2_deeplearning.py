34b75a7ae3e51c0d8adb5a41b4a94c1e659bb457
olyuninv <olyuninv@tcd.ie>
2018-11-19 08:07:41 +0000

    Initial commit

---
+++ Assignment2_deeplearning.py
@@ -0,0 +1,209 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 15 10:44:45 2018
+
+@author: olyuninv
+"""
+
+from __future__ import print_function
+import argparse
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torchvision import datasets, transforms
+import matplotlib.pyplot as plt
+
+# Training hyperparameters
+epochs = 10
+batch_size = 64
+learning_rate = 0.01
+momentum = 0.5
+log_interval = 20  
+
+class CNN(nn.Module):
+    def __init__(self):
+        super(CNN, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=16, 
+                               kernel_size=5, stride=1, padding=0)
+        self.conv2 = nn.Conv2d(in_channels=16, out_channels=8, 
+                               kernel_size=3, stride=1, padding=0)
+        self.fc1 = nn.Linear(in_features=800, out_features=128)
+        self.fc2 = nn.Linear(in_features=128, out_features=10)
+
+        nn.init.kaiming_normal_(self.conv1.weight)
+        nn.init.kaiming_normal_(self.conv2.weight)
+        nn.init.kaiming_normal_(self.fc1.weight)
+        nn.init.kaiming_normal_(self.fc2.weight)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = F.max_pool2d(x, kernel_size=2)        
+        x = F.relu(x)
+        x = self.conv2(x)
+        x = F.max_pool2d(x, kernel_size=2)
+        x = F.relu(x)
+        x = x.view(-1, 800)
+        x = self.fc1(x)
+        x = F.relu(x)
+        x = self.fc2(x)
+        return F.log_softmax(x, dim=1)
+
+def plot_data(data, label, text):
+    fig = plt.figure()
+    for i in range(6):
+        plt.subplot(2,3,i+1)
+        plt.tight_layout()
+        plt.imshow(data[i][0], cmap='gray', interpolation='none')
+        plt.title(text + ": {}".format(label[i]))
+        plt.xticks([])
+        plt.yticks([])
+    plt.show()
+	
+def predict_batch(model, device, test_loader):
+    examples = enumerate(test_loader)
+    model.eval()
+    with torch.no_grad():
+        batch_idx, (data, target) = next(examples)
+        data, target = data.to(device), target.to(device)
+        output = model(data)
+        pred = output.cpu().data.max(1, keepdim=True)[1] # get the index of the max log-probability
+        pred = pred.numpy()
+    return data, target, pred
+
+def plot_graph(train_x, train_y, test_x, test_y, ylabel=''):
+    fig = plt.figure()
+    plt.plot(train_x, train_y, color='blue')
+    plt.plot(test_x, test_y, color='red')
+    plt.legend(['Train', 'Test'], loc='upper right')
+    plt.xlabel('number of training examples seen')
+    plt.ylabel(ylabel)
+    plt.grid()
+    plt.show()
+
+def train(model, device, train_loader, optimizer, epoch, losses=[], counter=[], errors=[]):
+    model.train()
+    correct=0
+    for batch_idx, (data, target) in enumerate(train_loader):
+        data, target = data.to(device), target.to(device)
+        optimizer.zero_grad()
+        output = model(data)
+        loss = F.nll_loss(output, target)
+        loss.backward()
+        optimizer.step()
+        if batch_idx % log_interval == 0:
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                epoch, batch_idx * len(data), len(train_loader.dataset),
+                100. * batch_idx / len(train_loader), loss.item()))
+            losses.append(loss.item())
+            counter.append((batch_idx*batch_size) + ((epoch-1)*len(train_loader.dataset)))
+        pred = output.max(1, keepdim=True)[1]
+        correct += pred.eq(target.view_as(pred)).sum().item()
+    errors.append(100. * (1 - correct / len(train_loader.dataset)))
+
+def test(model, device, test_loader, losses=[], errors=[]):
+    model.eval()
+    test_loss = 0
+    correct = 0
+    with torch.no_grad():
+        for data, target in test_loader:
+            data, target = data.to(device), target.to(device)
+            output = model(data)
+            test_loss += F.nll_loss(output, target, reduction='sum').item() # sum up batch loss
+            pred = output.max(1, keepdim=True)[1] # get the index of the max log-probability
+            correct += pred.eq(target.view_as(pred)).sum().item()
+
+    test_loss /= len(test_loader.dataset)
+    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+        test_loss, correct, len(test_loader.dataset),
+        100. * correct / len(test_loader.dataset)))
+    losses.append(test_loss)
+    errors.append(100. *  (1 - correct / len(test_loader.dataset)))
+  
+def main():
+    use_cuda = torch.cuda.is_available()
+    device = torch.device("cuda" if use_cuda else "cpu")
+
+    # data transformation
+    train_data = datasets.FashionMNIST('../dataFashion', train=True, download=True,
+                   transform=transforms.Compose([
+                       transforms.ToTensor(),
+                       transforms.Normalize((0.1307,), (0.3081,))
+                   ]))
+
+    # Shapes of training set
+    print("Training set (images) shape: {shape}".format(shape=train_data.train_data.shape))
+    print("Training set (labels) shape: {shape}".format(shape=train_data.train_labels.shape))
+
+    test_data = datasets.FashionMNIST('../dataFashion', train=False, 
+                   transform=transforms.Compose([
+                       transforms.ToTensor(),
+                       transforms.Normalize((0.1307,), (0.3081,))
+                   ]))
+    # Shapes of test set
+    print("Test set (images) shape: {shape}".format(shape=test_data.test_data.shape))
+    print("Test set (labels) shape: {shape}".format(shape=test_data.test_labels.shape))
+
+    label_dict = {
+         0: 'T-shirt/top',
+         1: 'Trouser',
+         2: 'Pullover',
+         3: 'Dress',
+         4: 'Coat',
+         5: 'Sandal',
+         6: 'Shirt',
+         7: 'Sneaker',
+         8: 'Bag',
+         9: 'Ankle boot'
+    }
+
+    # data loaders
+    kwargs = {'num_workers': 1, 'pin_memory': True} if use_cuda else {}
+    train_loader = torch.utils.data.DataLoader(train_data, batch_size=batch_size, shuffle=True, **kwargs)
+    test_loader = torch.utils.data.DataLoader(test_data, batch_size=batch_size, shuffle=False, **kwargs)
+
+	# extract and plot random samples of data
+    examples = enumerate(test_loader)
+    batch_idx, (data, target) = next(examples)
+    data_labels = []
+
+    for i in range(0,10):
+        label_index = target[i].item()
+        label = '%i - %s' % (label_index, label_dict[label_index])
+        data_labels.append(label)        
+
+    plot_data(data, data_labels, 'GT')
+	
+    # model creation    
+    model = CNN().to(device)        
+    
+    # optimizer creation    
+    optimizer = optim.SGD(model.parameters(), lr=learning_rate, momentum=momentum, weight_decay = 0.001)
+
+    # lists for saving history
+    train_losses = []
+    train_counter = []
+    test_losses = []
+    test_counter = [i*len(train_loader.dataset) for i in range(epochs + 1)]
+    train_errors = []
+    test_errors = []
+    error_counter = [i*len(train_loader.dataset) for i in range(epochs)]
+
+    # test of randomly initialized model
+    test(model, device, test_loader, losses=test_losses)
+
+    # global training and testing loop
+    for epoch in range(1, epochs + 1):
+        train(model, device, train_loader, optimizer, epoch, losses=train_losses, counter=train_counter, errors=train_errors)
+        test(model, device, test_loader, losses=test_losses, errors=test_errors)
+		
+    # plotting training history
+    plot_graph(train_counter, train_losses, test_counter, test_losses, ylabel='negative log likelihood loss')
+    plot_graph(error_counter, train_errors, error_counter, test_errors, ylabel='error (%)')
+	
+    # extract and plot random samples of data with predicted labels
+    data, _, pred = predict_batch(model, device, test_loader)
+    plot_data(data, pred, 'Predicted')
+	    
+if __name__ == '__main__':
+    main()
